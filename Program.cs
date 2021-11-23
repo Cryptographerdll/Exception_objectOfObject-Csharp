dd6e1565b06c46d9eabb5c2bf702eca29df45ec0
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-11-23 19:20:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exception_objectOfObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int x = 10;
+                int y = 0;
+
+                int result = x / y;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
